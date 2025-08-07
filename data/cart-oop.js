445854e4097a49a8befb0d734a06d4918e3425fc
Trinh Nhat Huy <hwitn1610@gmail.com>
2025-08-07 16:23:43 +0700

    Create class version of cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -3,7 +3,7 @@ function Cart(localStorageKey) {
 		cartItems: undefined,
 
 		loadFromStorage() {
-			this.cartItems = JSON.parse(localStorage.getItem('localStorageKey'));
+			this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
 
 
 			if (!this.cartItems) {
@@ -20,7 +20,7 @@ function Cart(localStorageKey) {
 		},
 
 		saveToStorage() {
-			localStorage.setItem('localStorageKey',JSON.stringify(this.cartItems));
+			localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems));
 		},
 
 		addToCart(productId) {

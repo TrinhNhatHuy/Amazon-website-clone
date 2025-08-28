3812102bbf22d210cfb1d9909f25c11bb681616c
Trinh Nhat Huy <hwitn1610@gmail.com>
2025-08-28 12:19:54 +0700

    Making searching bar do its job

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,18 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
@@ -85,5 +96,11 @@ function renderProductsGrid() {
         addToCart(productId, quantity);
         updateCartQuantity();
       });
+    });
+
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
     });
 }
